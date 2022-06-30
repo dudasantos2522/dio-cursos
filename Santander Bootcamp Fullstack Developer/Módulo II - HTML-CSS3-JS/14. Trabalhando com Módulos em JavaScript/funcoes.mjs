840789ffcca749dac71531936df41a7fa546f88f
dudasantos2522 <mariaeduardasantos0205@gmail.com>
2022-06-30 14:01:13 -0300

    Módulo II - Manipulando a D.O.M. com JavaScript finalizado

---
+++ Santander Bootcamp Fullstack Developer/Módulo II - HTML-CSS3-JS/14. Trabalhando com Módulos em JavaScript/funcoes.mjs
@@ -0,0 +1,17 @@
+function idade(nome, idade) {
+    return `${nome} tem ${idade} anos`;
+}
+
+function cidade(nome, cidade) {
+    return `${nome} é de ${cidade}`;
+}
+
+function hobby(nome, hobby) {
+    return `${nome} gosta de ${hobby}`;
+}
+
+export {
+    idade,
+    cidade,
+    hobby
+}
---
+++ Santander Bootcamp Fullstack Developer/Módulo II - HTML-CSS3-JS/14. Trabalhando com Módulos em JavaScript/modulos.mjs
@@ -0,0 +1,5 @@
+import { idade, cidade, hobby } from "./funcoes.mjs";
+
+console.log(idade("Maria", 20));
+console.log(cidade("Maria", "São Carlos"));
+console.log(hobby("Maria", "séries"));
